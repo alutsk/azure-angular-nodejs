33caa5b07baeca88458e7a7ff8fe1b3787d0d775
Artem Lutsko <artem.lutsko@vivint.com>
2025-06-10 00:50:48 +0300

    used csp

--- server.js
+++ server.js
@@ -8,34 +8,64 @@ app.use(express.static(path.join(__dirname, 'dist', 'angular-express-azure', 'br
 
 // Handle all routes by serving the index.html file
 app.get('*', (req, res) => {
-  res.setHeader('X-Frame-Options', 'DENY');
-  res.sendFile(path.join(__dirname, 'dist', 'angular-express-azure', 'browser', 'index.html'));
+  // res.setHeader('X-Frame-Options', 'DENY');
+  // res.sendFile(path.join(__dirname, 'dist', 'angular-express-azure', 'browser', 'index.html'));
 
   // console.log('NONCE token: ', null);
 
 
 
-  // // path to file
-  // const filePath = path.resolve(__dirname, 'dist', 'browser', 'index.html');
+  // path to file
+  const filePath = path.resolve(__dirname, 'dist', 'browser', 'index.html');
 
-  // // read the file
-  // fs.readFile(filePath, 'utf8', function (err, data) {
-  //   if (err) {
-  //       return console.log(err);
-  //   }
+  // read the file
+  fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
 
-  //   // generate nonce and set it on header
-  //   // res.locals.nonce = uuidv4().replace(/\-/g, '');
-  //   // const nonce = uuidv4().replace(/\-/g, '');
-  //   // res.header('Content-Security-Policy', "default-src 'self';style-src 'self' `'nonce-${res.locals.nonce}'`;"
+    // generate nonce and set it on header
+    // res.locals.nonce = uuidv4().replace(/\-/g, '');
+    // const nonce = uuidv4().replace(/\-/g, '');
+    // // replace the special string with nonce
+    // const result = data.replace('NODE_CSP_NONCE', nonce);
 
-  //   // replace the special string with nonce
-  //   // const result = data.replace('NODE_CSP_NONCE', nonce);
-  //   console.log('NONCE token: ', nonce);
-  //   res.send(result);
-  // });
+    const { cspHeader, cspContent } = useCSP(data);
+
+    // console.log('NONCE token: ', nonce);
+    console.log('Request path: ', req.path);
+
+    res.header('Content-Security-Policy', cspHeader);
+    res.send(cspContent);
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+function useCSP(content) {
+  const nonce = uuidv4().replace(/\-/g, '');
+
+    // TODO: figure out how to extract from environment
+  const gatewayApiEndpoint = 'https://dev-gateway-apim.azure-api.net';
+  const stripeLibEndpoint = 'https://js.stripe.com/v3/';
+  const blobStorage = 'https://devstorageaccstg.blob.core.windows.net';
+
+  const defaultSrc = `default-src 'self' ${gatewayApiEndpoint}`;
+  const connectSrc = `connect-src 'self' https://maps.googleapis.com ${gatewayApiEndpoint}`;
+  const scriptSrc = `script-src 'self' https://maps.googleapis.com ${stripeLibEndpoint} 'nonce-${nonce}'`;
+  const styleSrc = `style-src 'self' 'nonce-${nonce}'`;
+  const imgSrc = `img-src 'self' data: ${blobStorage}`;
+  const frameSrc = `frame-src 'self' ${stripeLibEndpoint}`;
+  const frameAncestor = `frame-ancestors 'none'`;
+
+  console.log('NONCE token: ', nonce);
+  // console.log('Request path: ', req.path);
+
+  return {
+    // cspHeader: `${defaultSrc}; ${connectSrc}; ${styleSrc}; ${scriptSrc}; ${imgSrc}; ${frameSrc}; ${frameAncestor};`,
+    cspHeader: `default-src 'self' 'nonce-${nonce}'`,
+    cspContent: content.replace('NODE_CSP_NONCE', nonce),
+  }
+}
